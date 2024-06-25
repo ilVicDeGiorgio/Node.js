e6f0b4ab6061fd5292f87597cbf8959cdcf1665f
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-06-25 23:20:18 +0200

    Aggiustato Exercizio

--- my-figlet-project/src/server.js
+++ my-figlet-project/src/server.js
@@ -7,10 +7,8 @@ const port = 3000;
 // Middleware per il parsing del corpo delle richieste
 app.use(express.json());
 
-// Usa il router dei pianeti per tutte le rotte /api/planets
 app.use("/", planetRouter);
 
-// Avvio del server
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
